d6ac15a2fce9374e58292d68bdda473dec0f3615
Andrey <andrey.risukhin@outlook.com>
2021-08-23 10:35:15 -0700

    More test cases for get_win_matrix

--- tests/testthat/test-get_win_matrix.R
+++ tests/testthat/test-get_win_matrix.R
@@ -1,9 +1,12 @@
 dice_set_a <- list(c(1, 2, 3, 4, 5, 6), c(1, 2, 3, 4, 5, 6))
+dice_set_b <- c(1, 2, 3, 4, 5, 6)
+
 dice_ids_a <- c("Dice One")
 dice_ids_b <- c("Dice One", "Dice Two")
 dice_ids_c <- "should be vector"
+dice_ids_d <- c()
 
-test_that("lengths must match", {
+test_that("lengths of dice_set, dice_ids must match", {
   expect_error(get_win_matrix(dice_set_a, dice_ids_a))
 })
 
@@ -11,3 +14,16 @@ test_that("dice_ids should be vector", {
   expect_error(get_win_matrix(dice_set_a, dice_ids_c))
 })
 
+test_that("dice_ids should be a nonempty vector", {
+  expect_error(get_win_matrix(dice_set_a, dice_ids_d))
+})
+
+test_that("dice_set should be a list of vectors", {
+  expect_error(get_win_matrix(dice_set_b, dice_ids_a))
+})
+
+test_that("get_win_matrix should return a dataframe coded as a list", {
+  expect_type(get_win_matrix(dice_set_a, dice_ids_b), "list")
+})
+
+# Todo: Check returned dataframe dimensions make sense
